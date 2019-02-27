748d0ffbfb0a8725f446f312d23dae965c568556
zengkan <zengkan0703@163.com>
2019-02-28 00:27:41 +0800

    栈

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import LoopLinkList from './loop-link-list/index';
 import LinkList from './link-list';
-import {ring, ring2, ring3} from './joseph-ring'
+import { ring, ring2, ring3 } from './joseph-ring';
+import Stack from './stack';
+import { baseConverter } from './stack/example';
 
 window.LinkList = LinkList
 window.LoopLinkList = LoopLinkList;
 window.ring = ring;
 window.ring2 = ring2;
 window.ring3 = ring3;
+window.Stack = Stack;
+
+baseConverter(15, 16)
---
+++ src/stack/index.js
@@ -0,0 +1,28 @@
+export default class Stack {
+	constructor(arr = []) {
+		this.items = arr;
+	}
+	push(ele) {
+		this.items.push(ele);
+		return this.items;
+	}
+	pop() {
+		return this.items.pop();
+	}
+	peek() {
+		return this.items[this.items.length - 1];
+	}
+	isEmpty() {
+		return this.items.length === 0;
+	}
+	clear() {
+		this.item = [];
+		return this.items;
+	}
+	size() {
+		return this.items.length
+	}
+	print() {
+		return this.items.toString();
+	}
+}
