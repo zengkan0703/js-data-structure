e38f36f30b762874fb90abea7b5dc7eb1a48c260
zengkan <zengkan>
2019-02-27 15:58:42 +0800

    添加 webpack 方便测试

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import LoopLinkList from './loop-link-list/index';
+import LinkList from './linkList';
+
+window.LinkList = LinkList
+window.LoopLinkList = LoopLinkList;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  mode: "development",
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, '../src'),
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    hot: true,
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|mjs)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
