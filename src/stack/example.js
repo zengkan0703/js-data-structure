eed469e78c29e0961a4b928f17b7a4afb782d043
zengkan <zengkan>
2019-03-01 18:13:29 +0800

    三根柱子的汉诺塔递归

--- src/stack/example.js
+++ src/stack/example.js
@@ -37,6 +37,42 @@ const bracketBalance = (bracket) => {
 	return balance;
 }
 
+//汉诺塔问题
+/**
+ * 汉诺塔的层级
+ * @param {number} num 
+ */
+const hanoiTower = (num) => {
+	const arr = Array.from({length: num}).map((a, i) => i + 1);
+	const stackA = new Stack(arr);
+	const stackB = new Stack();
+	const stackC = new Stack();
+	let step = 0;
+
+	while(stackC.size() < num) {
+		const item = stackA.pop();
+		step ++;
+	}
+	return step;
+}
+
+//递归汉诺塔
+const hanoiTower2 = (num) => {
+	let step = 0;
+	const loop = (n, a, b, c) => {
+		if (n === 1) {
+			step ++;
+			// console.log(`${a} ==> ${c}`)
+		} else {
+			loop(n - 1, a, c, b);
+			loop(1, a, b, c);
+			loop(n - 1, b, a, c);
+		}
+	}
+	loop (num, 'A', 'B', 'C')
+	console.log(step, 'step 55555')
+}
+
 export {
 	baseConverter, bracketBalance
 }
