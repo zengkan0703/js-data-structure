e800b764c025a2791877ec0258c6f00b9b87c925
zengkan <zengkan>
2019-03-01 16:05:30 +0800

    利用栈解决判断括号是否平衡的问题

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import LoopLinkList from './loop-link-list/index';
 import LinkList from './link-list';
 import { ring, ring2, ring3 } from './joseph-ring';
 import Stack from './stack';
-import { baseConverter } from './stack/example';
+import { baseConverter, bracketBalance } from './stack/example';
 
 window.LinkList = LinkList
 window.LoopLinkList = LoopLinkList;
@@ -10,5 +10,6 @@ window.ring = ring;
 window.ring2 = ring2;
 window.ring3 = ring3;
 window.Stack = Stack;
+window.bracketBalance = bracketBalance;
 
 baseConverter(15, 16)
--- src/stack/example.js
+++ src/stack/example.js
@@ -16,6 +16,27 @@ const baseConverter = (num, base) => {
 	return value;
 }
 
+
+//判断括号是否平衡
+const bracketBalance = (bracket) => {
+	const leftBrackets = '({[';
+	const rightBrackets = ')}]';
+	const stack = new Stack();
+	let balance = true;
+	bracket.split('').map(d => {
+		if (leftBrackets.indexOf(d) !== -1) {
+			stack.push(d)
+		} else if (rightBrackets.indexOf(d) !== -1){
+			const left = stack.pop();
+			if (leftBrackets.indexOf(left) !== rightBrackets.indexOf(d)) {
+				balance = false
+			}
+		}
+	})
+	balance = balance && stack.isEmpty()
+	return balance;
+}
+
 export {
-	baseConverter
+	baseConverter, bracketBalance
 }
