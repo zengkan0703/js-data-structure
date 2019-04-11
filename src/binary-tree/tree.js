e300e8612116b400d3f32717bc291bf7dc8961e2
zengkan <zengkan>
2019-04-11 16:23:40 +0800

    二叉树更新

--- src/binary-tree/tree.js
+++ src/binary-tree/tree.js
@@ -1,40 +1,98 @@
 class Node {
-  constructor(data, left = null, right = null) {
-    this.data = data;
+  constructor(value, left = null, right = null, parent = null) {
+    this.value = value;
     this.left = left;
     this.right = right;
+    this.parent = parent;
+    this.height = 1;
+    this.size = 0;
   }
 }
 
+new Array(10).fill().map(d => parseInt(Math.random() * 100))
+
 //有序二叉树
 export default class Tree {
-  constructor(datas) {
+  constructor(values) {
     this.root = null;
-    datas.forEach(d => {
+    values.forEach(d => {
       this.insert(d)
     })
   }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode
+    } else {
+      this._insert(this.root, newNode)
+    }
+  }
   _insert(current, newNode) {
-    if (current.data > newNode.data) {
+    if (current.value > newNode.value) {
       if (current.left === null) {
-        current.left = newNode
+        newNode.parent = current;
+        current.left = newNode;
+        this._updateHeight(newNode);
+        this._updateSize(newNode);
       } else {
         this._insert(current.left, newNode)
       }
     } else {
       if (current.right === null) {
-        current.right = newNode
+        newNode.parent = current;
+        current.right = newNode;
+        this._updateHeight(newNode);
+        this._updateSize(newNode);
       } else {
         this._insert(current.right, newNode)
       }
     }
   }
-  insert(data) {
-    const newNode = new Node(data);
-    if (this.root === null) {
-      this.root = newNode
+  //递归更新上级树高度
+  _updateHeight(node) {
+    while(node.parent) {
+      const parent = node.parent;
+      const left = parent.left;
+      const right = parent.right;
+      const leftHeight = left ? left.height : 0;
+      const rightHeight = right ? right.height : 0;
+      const height = Math.max(leftHeight, rightHeight) + 1;
+      if (height === parent.height) {
+        break;
+      }
+      parent.height = height;
+      node = parent;
+    }
+  }
+  _updateSize(node) {
+    while(node.parent) {
+      const parent = node.parent;
+      const left = parent.left;
+      const right = parent.right;
+      const leftSize = left ? left.size + 1 : 0;
+      const rightSize = right ? right.size + 1 : 0;
+      const size = leftSize + rightSize;
+      if (size === parent.size) {
+        break;
+      }
+      parent.size = size;
+      node = parent;
+    }
+  }
+  //删除子树
+  remove(value) {
+    const node = this.search(value);
+    const { parent } = node;
+    if (parent) {
+      if (parent.value >= node.value) {
+        parent.left = null;
+      } else {
+        parent.right = null;
+      }
+      this._updateHeight(parent.left || parent.right);
+      this._updateSize(parent.left || parent.right);
     } else {
-      this._insert(this.root, newNode)
+      this.root = null;
     }
   }
   //先序遍历
@@ -99,25 +157,36 @@ export default class Tree {
       node = node.right
     }
   }
-  search(data) {
+  //根据值查找节点
+  search(value) {
     if (!this.root) {
       return
     }
-    return this._search(this.root, data)
+    return this._search(this.root, value)
   }
-  _search(node, data) {
+  _search(node, value) {
     if (!node) {
       console.log('没有相应的值')
       return 
     }
-    if (node.data === data) {
+    if (node.value === value) {
       return node
     }
-    if (node.data > data) {
-      return this._search(node.left, data)
+    if (node.value > value) {
+      return this._search(node.left, value)
     } else {
-      return this._search(node.right, data)
+      return this._search(node.right, value)
     }
+  }
+  //查找所有页节点
+  getLeaves() {
+    const leaves = [];
+    this.travPre(d => {
+      if (!d.right && !d.left) {
+        leaves.push(d)
+      }
+    }) 
+    return leaves;
   }
 }
 
