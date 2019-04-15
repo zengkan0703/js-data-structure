5296ed69bf4b1b38a54b0a8d63e8f24476ee9fca
zengkan <zengkan>
2019-04-15 20:23:11 +0800

    暂存

---
+++ src/array/index.js
@@ -0,0 +1,22 @@
+//数组去重
+//时间复杂度 
+const deduplicate = (arr) => {
+  return arr.reduce((p, n) => {
+    if (!p.find(a => a === n)) {
+      p.push(n);
+    }
+    return p;
+  }, [])
+}
+
+
+//有序数组去重
+//时间复杂度  n * 3 = O(n)
+const deduplicate2 = (arr) => {
+  return arr.reduce((p, n, i) => {
+    if (i > 0 && n !== arr[i - 1]) {
+      p.push(n);
+    }
+    return p;
+  }, [arr[0]])
+}
