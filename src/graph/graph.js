2065a16f30b44e2ea0fd42d75e1913149091878b
zengkan <zengkan0703@163.com>
2019-04-17 22:44:46 +0800

    暂存

--- src/graph/graph.js
+++ src/graph/graph.js
@@ -25,18 +25,57 @@ class Graph {
   }
   insertVertex (data) {
     const vertex = new Vertex(data);
-    this.edges.push(this.vertexs.map(v => 0));
+    this.edges.push(this.vertexs.map(v => null));
     this.edges.map(e => {
-      e.push(0)
+      e.push(null)
     })
     this.vertexs.push(vertex);
     return this.vertexs.length - 1;
   }
   removeVertex (idx) {
+    this.vertexs.map((v, i)=> {
+      if (this.edges[i][idx]) {
+        this.vertexs[i].outDegree --;
+      }
+      if (this.edges[idx][i]) {
+        this.vertexs[i].inDegree --;
+      }
+    })
     this.vertexs.splice(idx, 1);
     this.edges.splice(idx, 1);
     this.edges.map(e => {
       e.splice(idx, 1)
     })
+  }
+  //确认边是否存在
+  exist (i, j) {
+    try {
+      return !!this.edges[i][j];
+    } catch(err) {
+      return false;
+    }
+  }
+  insertEdge (data, weight, i, j) {
+    if (this.exist(i, j)) {
+      return;
+    }
+    const length = this.vertexs.length;
+    if (i < length || j < length) {
+      return;
+    }
+    this.edges[i][j] = new Edge(data, weight);
+    this.vertexs[i].outDegree ++;
+    this.vertexs[j].inDegree ++;
+  }
+  removeEdge (i, j) {
+    if (!this.exist(i, j)) {
+      return;
+    }
+    const length = this.vertexs.length;
+    if (i < length || j < length) {
+      return;
+    }
+    this.edges[i][j] = null;
+    
   }
 }
